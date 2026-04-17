766e0d5f110149dce1edbad5c0b461a7a6972f44
SergioTovar94 <sergiotovar9430@gmail.com>
2026-04-17 17:43:39 -0500

    feat:front list loan

--- Application/UseCases/Loans/ListLoansUseCase.cs
+++ Application/UseCases/Loans/ListLoansUseCase.cs
@@ -1,6 +1,20 @@
+using Application.Abstractions.Loans;
+using Domain.Entities;
+using Shared;
+
 namespace Application.UseCases.Loans;
 
 public class ListLoansUseCase
 {
+    private readonly ILoanRepository _loanRepository;
 
+    public ListLoansUseCase(ILoanRepository loanRepository)
+    {
+        _loanRepository = loanRepository;
+    }
+    public async Task<Result<IEnumerable<LoanEntity>>> Handle(ListLoansQuery _, CancellationToken ct)
+    {
+        var loans = await _loanRepository.GetAllAsync(ct);
+        return Result<IEnumerable<LoanEntity>>.Success(loans);
+    }
 }
